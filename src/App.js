39d791d57f2b85bf79b26c215415bfcd3eb2f933
favouritesir <102854760+favouritesir@users.noreply.github.com>
2022-11-20 12:02:21 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
 import logo from './logo.svg';
 import './App.css';
-
-function App() {
+import { useState } from 'react';
+function App(){
+  return (
+    <React/>
+  )
+}
+function React() {
+  
+  let p=[{nm:'ashik',id:'001'},{nm:'surieya',id:'002'} ]
+  
   return (
     <div className="App">
+      <Counter></Counter>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {p.map(p=><Comp val={p}></Comp>)}
       </header>
     </div>
   );
 }
+
+function Counter(){
+  const [value,setCount]=useState(0);
+  return(
+    <div>
+      <h1>Count: {value}    
+      <button onClick={()=>setCount((value<0)?value-1:0)}>-</button>
+      <button onClick={()=>setCount(value+1)}>+</button>
+      </h1>
+    </div>
+  );
+}
+
+function Comp(p){
+  let style={color:"red",border:"2px solid white",margin:"5px",padding:'5px'}
+  return(
+    <div style={style}>
+      <h1>Welcome to our react app</h1>
+      <p>Name: {p.val.nm}</p>
+      <p>id: {p.val.id}</p>
+
+    </div>
+  )
+}
+
 
 export default App;
